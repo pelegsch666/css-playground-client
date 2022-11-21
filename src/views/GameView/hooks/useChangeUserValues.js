dc50982e7b086870ef7158f9947dcfb31d8b4fc7
Irad Doron <irad.doron@gmail.com>
2022-11-21 19:38:44 +0200

    fix bug in dont-flex

--- src/views/GameView/hooks/useChangeUserValues.js
+++ src/views/GameView/hooks/useChangeUserValues.js
@@ -1,80 +1,89 @@
-import { useRecoilState, useRecoilValue } from "recoil";
+import { useRecoilState, useRecoilValue } from 'recoil';
 
-import propertiesValueStrings from "constants/propertiesValueStrings";
+import propertiesValueStrings from 'constants/propertiesValueStrings';
 
 import {
-  currPropertyTitleState,
-  currUserShapePropertyIdxState,
-  userShapeState,
-} from "views/GameView/store";
+	currPropertyTitleState,
+	currUserShapePropertyIdxState,
+	userShapeState,
+} from 'views/GameView/store';
 
 function isValueNumber(value) {
-  return typeof value === "number";
+	return typeof value === 'number';
 }
 
 function getNewValueBySignAndGap(value, sign, gap) {
-  let res;
-  if (sign === "+") {
-    res = value + gap;
-  } else if (sign === "-") {
-    res = value - gap;
-  }
-  return res;
+	let res;
+	if (sign === '+') {
+		res = value + gap;
+	} else if (sign === '-') {
+		res = value - gap;
+	}
+	return res;
 }
 
 function useChangeUserValues() {
-  const [userShape, setUserShape] = useRecoilState(userShapeState);
-  const currPropertyTitle = useRecoilValue(currPropertyTitleState);
-  const currPropIdx = useRecoilValue(currUserShapePropertyIdxState);
+	const [userShape, setUserShape] = useRecoilState(userShapeState);
+	const currPropertyTitle = useRecoilValue(currPropertyTitleState);
+	const currPropIdx = useRecoilValue(currUserShapePropertyIdxState);
 
-  function setter(sign) {
-    const newUserShape = [...userShape];
-    const newCurrProp = { ...newUserShape[currPropIdx][currPropertyTitle] };
+	function setter(sign) {
+		const newUserShape = [...userShape];
+		const newCurrProp = { ...newUserShape[currPropIdx][currPropertyTitle] };
 
-    if (isValueNumber(newCurrProp.value)) {
-      const { value, gap, min, max } = newCurrProp;
-      const newValue = getNewValueBySignAndGap(value, sign, gap);
-      if (newValue > max || newValue < min) {
-        newUserShape[currPropIdx] = { [currPropertyTitle]: { ...newCurrProp } };
+		if (isValueNumber(newCurrProp.value)) {
+			const { value, gap, min, max } = newCurrProp;
+			const newValue = getNewValueBySignAndGap(value, sign, gap);
+			if (newValue > max || newValue < min) {
+				console.log('newUserShape[currPropIdx]', newUserShape[currPropIdx]);
+				newUserShape[currPropIdx] = { [currPropertyTitle]: { ...newCurrProp } };
 
-        return setUserShape(newUserShape);
-      }
-      newCurrProp.value = newValue;
-      newUserShape[currPropIdx] = { [currPropertyTitle]: { ...newCurrProp } };
-      setUserShape(newUserShape);
-    } else {
-      const { value, items } = newUserShape[currPropIdx][currPropertyTitle];
-      const currIdx = items.indexOf(value);
-      const currProperty = newUserShape[currPropIdx][currPropertyTitle];
-      console.log("currProperty", currProperty);
-      if (sign === "+") {
-        if (currIdx === items.length - 1) {
-          const newIdx = 0;
-          currProperty.value = items[newIdx];
-          console.log("nuCurrProperty", currProperty);
-          setUserShape(newUserShape);
-        } else {
-          const newIdx = currIdx + 1;
-          currProperty.value = items[newIdx];
-          console.log("nuCurrProperty", currProperty);
-          setUserShape(newUserShape);
-        }
-      } else if (sign === "-") {
-        if (currIdx === 0) {
-          const newIdx = items - 1;
-          currProperty.value = items[newIdx];
-          console.log("nuCurrProperty", currProperty);
-          setUserShape(newUserShape);
-        } else {
-          const newIdx = currIdx - 1;
-          currProperty.value = items[newIdx];
-          console.log("nuCurrProperty", currProperty);
-          setUserShape(newUserShape);
-        }
-      }
-    }
-  }
-  return setter;
+				return setUserShape(newUserShape);
+			}
+			newCurrProp.value = newValue;
+			newUserShape[currPropIdx] = { [currPropertyTitle]: { ...newCurrProp } };
+			setUserShape(newUserShape);
+		} else {
+			const { value, items } = newUserShape[currPropIdx][currPropertyTitle];
+			const currIdx = items.indexOf(value);
+			const currProperty = newUserShape[currPropIdx][currPropertyTitle];
+			console.log('currProperty', currProperty);
+			if (sign === '+') {
+				if (currIdx === items.length - 1) {
+					const newIdx = 0;
+					const newValue = items[newIdx];
+					newUserShape[currPropIdx] = {
+						[currPropertyTitle]: { ...newCurrProp, value: newValue },
+					};
+					setUserShape(newUserShape);
+				} else {
+					const newIdx = currIdx + 1;
+					const newValue = items[newIdx];
+					newUserShape[currPropIdx] = {
+						[currPropertyTitle]: { ...newCurrProp, value: newValue },
+					};
+					setUserShape(newUserShape);
+				}
+			} else if (sign === '-') {
+				if (currIdx === 0) {
+					const newIdx = items.length - 1;
+					const newValue = items[newIdx];
+					newUserShape[currPropIdx] = {
+						[currPropertyTitle]: { ...newCurrProp, value: newValue },
+					};
+					setUserShape(newUserShape);
+				} else {
+					const newIdx = currIdx - 1;
+					const newValue = items[newIdx];
+					newUserShape[currPropIdx] = {
+						[currPropertyTitle]: { ...newCurrProp, value: newValue },
+					};
+					setUserShape(newUserShape);
+				}
+			}
+		}
+	}
+	return setter;
 }
 
 // function updateUserShape(newIdx) {
