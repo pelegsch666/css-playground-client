efa7a10e15d340564f0bd4416d3da0bb0458599c
pelegsch666 <peleg.mazuz@gmail.com>
2022-11-21 11:35:36 +0200

    still in work

--- src/levels/dont_flex.js
+++ src/levels/dont_flex.js
@@ -1,73 +1,78 @@
-import getRandomItemFromArray from 'helpers/getRandomItemFromArray';
+import getRandomItemFromArray from "helpers/getRandomItemFromArray";
 
 const dont_flex = {
-	levelName: 'Dont Flex',
-	levelId: 2,
-	renderingKey: 'Flex',
-	defaultProperties: {
-		display: 'flex',
-		width: 200,
-		height: 200,
-		backgroundColor: '#aaffaa',
-	},
-	targetProperties: {
-		justifyContent: {
-			items: [
-				'flex-start',
-				'flex-end',
-				'center',
-				'space-between',
-				'space-around',
-				'space-evenly',
-			],
-			get value() {
-				return getRandomItemFromArray(this.items);
-			},
-		},
+  levelName: "Dont Flex",
+  levelId: 2,
+  renderingKey: "Flex",
+  defaultProperties: {
+    display: "flex",
+    width: 200,
+    height: 200,
+    backgroundColor: "#aaffaa",
+  },
+  targetProperties: {
+    justifyContent: {
+      items: [
+        "flex-start",
+        "flex-end",
+        "center",
+        "space-between",
+        "space-around",
+        "space-evenly",
+      ],
 
-		alignItems: {
-			items: ['flex-start', 'flex-end', 'center', 'stretch', 'baseline'],
-			get value() {
-				return getRandomItemFromArray(this.items);
-			},
-		},
+      get value() {
+        return this.value;
+      },
+    },
 
-		flexDirection: {
-			items: ['row', 'row-reverse', 'column', 'column-reverse'],
-			get value() {
-				return getRandomItemFromArray(this.items);
-			},
-		},
-	},
-	userProperties: {
-		justifyContent: {
-			items: [
-				'flex-start',
-				'flex-end',
-				'center',
-				'space-between',
-				'space-around',
-				'space-evenly',
-			],
-			get value() {
-				return getRandomItemFromArray(this.items);
-			},
-		},
+    alignItems: {
+      items: ["flex-start", "flex-end", "center", "stretch", "baseline"],
+      get value() {
+        return this.value;
+      },
+    },
 
-		alignItems: {
-			items: ['flex-start', 'flex-end', 'center', 'stretch', 'baseline'],
-			get value() {
-				return getRandomItemFromArray(this.items);
-			},
-		},
+    flexDirection: {
+      items: ["row", "row-reverse", "column", "column-reverse"],
+      get value() {
+        return this.value;
+      },
+    },
+  },
+  userProperties: {
+    justifyContent: {
+      items: [
+        "flex-start",
+        "flex-end",
+        "center",
+        "space-between",
+        "space-around",
+        "space-evenly",
+      ],
+      value: '',
+      get value() {
+        return getRandomItemFromArray(this.items);
+      },
+	  set value(newValue) {
+		 this.value = newValue
+	  }
+    },
 
-		flexDirection: {
-			items: ['row', 'row-reverse', 'column', 'column-reverse'],
-			get value() {
-				return getRandomItemFromArray(this.items);
-			},
-		},
-	},
+    alignItems: {
+      items: ["flex-start", "flex-end", "center", "stretch", "baseline"],
+      get value() {
+        return this.value;
+      },
+    },
+
+    flexDirection: {
+      items: ["row", "row-reverse", "column", "column-reverse"],
+      get value() {
+        return getRandomItemFromArray(this.items);
+      },
+    },
+  },
 };
 
 export default dont_flex;
--- src/views/GameView/hooks/useChangeUserValues.js
+++ src/views/GameView/hooks/useChangeUserValues.js
@@ -1,104 +1,85 @@
-import { useRecoilState, useRecoilValue } from 'recoil';
+import { useRecoilState, useRecoilValue } from "recoil";
 
-import propertiesValueStrings from 'constants/propertiesValueStrings';
+import propertiesValueStrings from "constants/propertiesValueStrings";
 
 import {
-	currPropertyTitleState,
-	currUserShapePropertyIdxState,
-	userShapeState
-} from 'views/GameView/store';
+  currPropertyTitleState,
+  currUserShapePropertyIdxState,
+  userShapeState,
+} from "views/GameView/store";
 
 function isValueNumber(value) {
-	return typeof value === 'number';
+  return typeof value === "number";
 }
 
 function getNewValueBySignAndGap(value, sign, gap) {
-	let res;
-	if (sign === '+') {
-		res = value + gap;
-	} else if (sign === '-') {
-		res = value - gap;
-	}
-	return res;
+  let res;
+  if (sign === "+") {
+    res = value + gap;
+  } else if (sign === "-") {
+    res = value - gap;
+  }
+  return res;
 }
 
 function useChangeUserValues() {
-	const [userShape, setUserShape] = useRecoilState(userShapeState);
-	const currPropertyTitle = useRecoilValue(currPropertyTitleState);
-	const currPropIdx = useRecoilValue(currUserShapePropertyIdxState);
+  const [userShape, setUserShape] = useRecoilState(userShapeState);
+  const currPropertyTitle = useRecoilValue(currPropertyTitleState);
+  const currPropIdx = useRecoilValue(currUserShapePropertyIdxState);
 
-	function setter(sign) {
-		const newUserShape = [...userShape];
-		const newCurrProp = { ...newUserShape[currPropIdx][currPropertyTitle] };
+  function setter(sign) {
+    const newUserShape = [...userShape];
+    const newCurrProp = { ...newUserShape[currPropIdx][currPropertyTitle] };
 
-		if (isValueNumber(newCurrProp.value)) {
-			const { value, gap, min, max } = newCurrProp;
-			const newValue = getNewValueBySignAndGap(value, sign, gap);
-			if (newValue > max || newValue < min) {
-				newUserShape[currPropIdx] = { [currPropertyTitle]: { ...newCurrProp } };
-				return setUserShape(newUserShape);
-			}
-			newCurrProp.value = newValue;
-			newUserShape[currPropIdx] = { [currPropertyTitle]: { ...newCurrProp } };
-			setUserShape(newUserShape);
-		} else {
-			const { value, items } = newUserShape[currPropIdx][currPropertyTitle];
-			const currIdx = items.indexOf(value);
-			// console.log('value', value);
-			// console.log('items', items);
-			// console.log('currIdx', currIdx);
-			// console.log('newUserShape', newUserShape);
-			// console.log('currPropIdx', currPropIdx);
-			// console.log('currPropertyTitle', currPropertyTitle);
-			// console.log(
-			// 	'...newUserShape[currPropIdx][currPropertyTitle]',
-			// 	newUserShape[currPropIdx][currPropertyTitle]
-			// );
-			// console.log('value: items[newIdx]', items[currIdx + 1]);
-			if (sign === '+') {
-				if (currIdx === items.length - 1) {
-					const newIdx = 0;
-					newUserShape[currPropIdx][currPropertyTitle] = {
-						...newUserShape[currPropIdx][currPropertyTitle],
-						value: items[newIdx],
-					};
-					setUserShape(newUserShape);
-				} else {
-					const newIdx = currIdx + 1;
-					const newValue = items[newIdx];
-					console.log(newUserShape[currPropIdx][currPropertyTitle]);
+    if (isValueNumber(newCurrProp.value)) {
+      const { value, gap, min, max } = newCurrProp;
+      const newValue = getNewValueBySignAndGap(value, sign, gap);
+      if (newValue > max || newValue < min) {
+        newUserShape[currPropIdx] = { [currPropertyTitle]: { ...newCurrProp } };
 
-					console.log(newUserShape[currPropIdx][currPropertyTitle].value);
-					console.log('newUserShape', newUserShape);
-
-					newUserShape[currPropIdx][currPropertyTitle] = {
-						...newUserShape[currPropIdx][currPropertyTitle],
-					};
-
-					console.log('newUserShape', newUserShape);
-
-					setUserShape(newUserShape);
-				}
-			} else if (sign === '-') {
-				if (currIdx === 0) {
-					const newIdx = items - 1;
-					newUserShape[currPropIdx][currPropertyTitle] = {
-						...newUserShape[currPropIdx][currPropertyTitle],
-						value: items[newIdx],
-					};
-					setUserShape(newUserShape);
-				} else {
-					const newIdx = currIdx - 1;
-					newUserShape[currPropIdx][currPropertyTitle] = {
-						...newUserShape[currPropIdx][currPropertyTitle],
-						value: items[newIdx],
-					};
-					setUserShape(newUserShape);
-				}
-			}
-		}
-	}
-	return setter;
+        return setUserShape(newUserShape);
+      }
+      newCurrProp.value = newValue;
+      newUserShape[currPropIdx] = { [currPropertyTitle]: { ...newCurrProp } };
+      setUserShape(newUserShape);
+    } else {
+      const { value, items } = newUserShape[currPropIdx][currPropertyTitle];
+      const currIdx = items.indexOf(value);
+      const currProperty = newUserShape[currPropIdx][currPropertyTitle];
+      console.log("currProperty", currProperty);
+      if (sign === "+") {
+        if (currIdx === items.length - 1) {
+          const newIdx = 0;
+          currProperty.value = items[newIdx];
+          console.log("nuCurrProperty", currProperty);
+          setUserShape(newUserShape);
+        } else {
+          const newIdx = currIdx + 1;
+          currProperty.value = items[newIdx];
+          console.log("nuCurrProperty", currProperty);
+          setUserShape(newUserShape);
+        }
+      } else if (sign === "-") {
+        if (currIdx === 0) {
+          const newIdx = items - 1;
+          currProperty.value = items[newIdx];
+          console.log("nuCurrProperty", currProperty);
+          setUserShape(newUserShape);
+        } else {
+          const newIdx = currIdx - 1;
+          currProperty.value = items[newIdx];
+          console.log("nuCurrProperty", currProperty);
+          setUserShape(newUserShape);
+        }
+      }
+    }
+  }
+  return setter;
 }
+
+// function updateUserShape(newIdx) {
+//   currProperty.value = items[newIdx];
+//   setUserShape(newUserShape);
+// }
 
 export default useChangeUserValues;
